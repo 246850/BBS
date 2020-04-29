b78b96f49a93d0e16ce32624533592ce706084ad
246850 <390915549@qq.com>
2020-04-29 09:27:21 +0800

    linq 动态排序

--- BBS.Framework/Extensions/EnumerableExtensions.cs
+++ BBS.Framework/Extensions/EnumerableExtensions.cs
@@ -26,5 +26,47 @@ namespace BBS.Framework.Extensions
         {
             return condition ? queryable.Where(predicate) : queryable;
         }
+
+        /// <summary>
+        /// 动态排序 - 根据字段名转表达式
+        /// </summary>
+        /// <typeparam name="TSource">实体类型</typeparam>
+        /// <param name="queryable">查询表达式</param>
+        /// <param name="propertyName">属性名</param>
+        /// <param name="asc">true:升序，false:降序</param>
+        /// <returns></returns>
+        public static IOrderedQueryable<TSource> OrderBy<TSource>(this IQueryable<TSource> queryable, string propertyName, bool asc)
+             where TSource : class
+        {
+            if (queryable == null)
+                throw new ArgumentNullException(nameof(queryable));
+            if (string.IsNullOrWhiteSpace(propertyName))
+                throw new ArgumentNullException(nameof(propertyName));
+
+            var propertyInfo = queryable.ElementType.GetProperty(propertyName);
+            if (propertyInfo == null)
+                throw new ArgumentException($"{nameof(propertyName)}不是类{queryable.ElementType.FullName}的属性字段", propertyName);
+
+            ParameterExpression parameter = Expression.Parameter(typeof(TSource), "x");
+            MemberExpression property = Expression.Property(parameter, propertyInfo);
+            LambdaExpression sort = Expression.Lambda(property, parameter);
+
+            MethodCallExpression call = Expression.Call(
+                                    typeof(Queryable),
+                                    asc ?"OrderBy" : "OrderByDescending",
+                                    new[] { typeof(TSource), property.Type },
+                                    queryable.Expression,
+                                    Expression.Quote(sort));
+
+            return (IOrderedQueryable<TSource>)queryable.Provider.CreateQuery<TSource>(call);
+        }
+
+        public static Expression<Func<TSource, TKey>> BuildExpression<TSource, TKey>(this string propertyName)
+            where TSource:class
+        {
+            ParameterExpression param = Expression.Parameter(typeof(TSource), "x");
+            Expression conversion = Expression.Convert(Expression.Property(param, propertyName), typeof(TKey));   //important to use the Expression.Convert
+            return Expression.Lambda<Func<TSource, TKey>>(conversion, param);
+        }
     }
 }
